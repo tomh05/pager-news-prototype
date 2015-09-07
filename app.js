e6d986162f5d5ba1a3eb3393f226e66c0e89a578
Tom <tom.howe@bbc.co.uk>
2015-09-07 15:42:21 +0100

    start of drawers implemented

--- app.js
+++ app.js
@@ -8,40 +8,42 @@ var SpineElement = function(mainImgSrc) {
 
 };
 
-var MoreItem = function(type,contentImg,buttonImg) {
-    this.type = type;
+var MoreItem = function(spineElementNo,moreItemNo,contentImg,buttonImg) {
     this.buttonLayer = new Layer({x:0, y:0, width:1027, height:600, image:buttonImg});
+    this.buttonLayer.spineElementNo = spineElementNo;
+    this.buttonLayer.moreItemNo = moreItemNo;
     this.contentImg = contentImg;
-        //this.contentLayer = new Layer({x:0, y:0, width:1027, height:600, image:contentImg})
-        //
-    if (this.type == "popup") {
-        this.buttonLayer.on(Events.TouchEnd, function(event, layer) {
-            if (scrolling) return;
-            // create popup
-            this.popup = new Popup(contentImg);
-            event.stopPropagation();
-        });
-    }
+    //this.contentLayer = new Layer({x:0, y:0, width:1027, height:600, image:contentImg})
+
+    this.buttonLayer.on(Events.TouchEnd, function(event, layer) {
+        if (scrolling) return;
+        // create popup
+        //this.popup = new Popup(contentImg);
+        var spineElement = spineElements[layer.spineElementNo];
+        spineElement.moreSection = new MoreSection(spineElement.moreItems,layer.spineElementNo);
+        event.stopPropagation();
+    });
 };
 
-var Popup = function(imgPath) {
+var MoreBox = function(imgPath) {
 
-    this.containerLayer = new Layer({x:60, y:60, width:1320, height:2244, image:"images/popup_container.jpg"});
+    this.layer = new Layer({x:60, y:60, width:1020, height:2244, image:"images/popup_container.jpg"});
 
     this.closeButton = new Layer({x:1090, y:20, width:100, height:100, 
         image:"images/icon.png",
-        superLayer: this.containerLayer
+        superLayer: this.layer
     });
 
     // set up scrollers
     this.scroller = new Framer.ScrollComponent({
-        x:0, y:100,
-        width: 1320,
+        x:10, y:100,
+        width: 1000,
         height: 1900,
         scrollVertical: true,
         scrollHorizontal: false,
+        directionLock: true,
         y: 100,
-        superLayer: this.containerLayer
+        superLayer: this.layer
     });
 
     this.contentLayer = new Layer({
@@ -51,16 +53,46 @@ var Popup = function(imgPath) {
         superLayer: this.scroller.content
     });
 
-        this.closeButton.on(Events.TouchEnd, function(event, layer) {
-            // destroy popup
-            layer.superLayer.destroy();
-           console.log("clicky");
-            event.stopPropagation();
-        });
+    this.closeButton.on(Events.TouchEnd, function(event, layer) {
+        layer.superLayer.destroy();
+        event.stopPropagation();
+    });
 };
 
-SpineElement.prototype.addMoreItem = function(type,contentImg,buttonImg) {
-    var moreItem = new MoreItem(type,contentImg,buttonImg);
+var MoreSection = function(moreItems,startingElementNo) {
+    this.moreItems = moreItems;
+    this.moreBoxes = new Array();
+
+        console.log(this);
+    // create page controller
+    this.pager = new Framer.PageComponent({
+        width: 1240,
+        height: 2000,
+        scrollVertical: false,
+        scrollHorizontal: true,
+        y: 100,
+        propagateEvents: false,
+        directionLock: true,
+        backgroundColor: "transparent",
+        directionLockThreshold: {x:10,y:10},
+        contentInset: {top:100,right:100,bottom:100,left:100}
+    //superLayer: spineElement.layer
+    }); 
+
+    var self = this;
+    this.moreItems.forEach(function(moreItem,index,array){
+        // create a box
+        console.log("creating box "+index);
+        var moreBox = new MoreBox(moreItem.contentImg);
+        self.moreBoxes.push(moreBox);
+        self.pager.addPage(moreBox.layer,"right");
+    });
+
+};
+
+SpineElement.prototype.addMoreItem = function(spineElementNo,contentImg,buttonImg) {
+    var moreItemNo = this.moreItems.length;
+    var moreItem = new MoreItem(spineElementNo, moreItemNo, contentImg,buttonImg);
     //var morebutton = new Layer({x:0, y:0, width:1027, height:600, image:imgSrc})
     this.moreItems.push(moreItem);
 };
@@ -71,28 +103,28 @@ spineElements.push( new SpineElement("images/01.jpg") );
 spineElements.push( new SpineElement("images/02.jpg") ); 
 spineElements.push( new SpineElement("images/03.jpg") ); 
 
-spineElements[0].addMoreItem("deeper", "images/1.1.jpg","images/01_strip_1.jpg");
-spineElements[0].addMoreItem("deeper", "images/1.1.jpg", "images/01_strip_2.jpg");
-spineElements[1].addMoreItem("deeper", "images/02.1.jpg", "images/02_strip_1.jpg");
-spineElements[1].addMoreItem("popup", "images/POP_2.1.jpg", "images/02_strip_2.jpg");
-spineElements[1].addMoreItem("popup", "images/POP_2.2.jpg", "images/02_strip_3.jpg");
+spineElements[0].addMoreItem(0, "images/1.1.jpg","images/01_strip_1.jpg");
+spineElements[0].addMoreItem(0, "images/1.1.jpg", "images/01_strip_2.jpg");
+spineElements[1].addMoreItem(1, "images/02.1.jpg", "images/02_strip_1.jpg");
+spineElements[1].addMoreItem(1, "images/POP_2.1.jpg", "images/02_strip_2.jpg");
+spineElements[1].addMoreItem(1, "images/POP_2.2.jpg", "images/02_strip_3.jpg");
 /*
-   //atoms
+//atoms
 atom_1 = new Layer({x:0, y:0, width:1440, height:2392, image:"images/01.jpg"})
 atom_2 = new Layer({x:0, y:0, width:1440, height:2392, image:"images/02.jpg"})
 atom_3 = new Layer({x:0, y:0, width:1440, height:2392, image:"images/03.jpg"})
-   //more buttons
+//more buttons
 atom_1_strip_1 = new Layer({x:0, y:0, width:1027, height:600, image:"images/01_strip_1.jpg"})
 atom_1_strip_2 = new Layer({x:0, y:0, width:1027, height:600, image:"images/01_strip_2.jpg"})
 atom_2_strip_1 = new Layer({x:0, y:0, width:1027, height:600, image:"images/02_strip_1.jpg"})
 atom_2_strip_2 = new Layer({x:0, y:0, width:1027, height:600, image:"images/02_strip_2.jpg"})
 atom_2_strip_3 = new Layer({x:0, y:0, width:1027, height:600, image:"images/02_strip_3.jpg"})
 
-   //sub-atoms
+//sub-atoms
 atom_1_1 = new Layer({x:0, y:0, width:1318, height:4178, image:"images/1.1.jpg"})
 atom_2_1 = new Layer({x:0, y:0, width:1320, height:2120, image:"images/02.1.jpg"})
 
-   //popups
+//popups
 pop_2_1 = new Layer({x:20, y:20, width:1401, height:2569, image:"images/POP_2.1.jpg"})
 pop_2_2 = new Layer({x:20, y:20, width:1401, height:2608, image:"images/POP_2.2.jpg"})
 pop_3_1 = new Layer({x:20, y:20, width:1401, height:2791, image:"images/POP_3.1.jpg"})
@@ -105,12 +137,12 @@ var scrolling = false;
 //////////////////////////////////////////////////////////////////
 // make main pager
 var mainPager = new Framer.PageComponent({
-        width: 1440,
-          height: 2392,//1720,//2392,
-          scrollVertical: false,
-          scrollHorizontal: true,
-          y: 0//,
-    });
+    width: 1440,
+    height: 2392,//1720,//2392,
+    scrollVertical: false,
+    scrollHorizontal: true,
+    y: 0//,
+});
 
 
 spineElements.forEach(function(spineElement,index,array){
@@ -118,15 +150,15 @@ spineElements.forEach(function(spineElement,index,array){
 
     spineElement.drawer = new Framer.PageComponent({
         width: 1440,
-          height: 600,
-          scrollVertical: false,
-          scrollHorizontal: true,
-          y: 1720,
-          propagateEvents: false,
-          directionLock: true,
-          backgroundColor: "transparent",
-          directionLockThreshold: {x:10,y:10},
-          superLayer: spineElement.layer
+        height: 600,
+        scrollVertical: false,
+        scrollHorizontal: true,
+        y: 1720,
+        propagateEvents: false,
+        directionLock: true,
+        backgroundColor: "transparent",
+        directionLockThreshold: {x:10,y:10},
+        superLayer: spineElement.layer
     }); 
     spineElement.drawer.on(Events.DirectionLockDidStart, function(event, layer) {  scrolling = true; });
     spineElement.drawer.on(Events.ScrollEnd, function(event, layer) {  scrolling = false; });
@@ -143,71 +175,71 @@ spineElements.forEach(function(spineElement,index,array){
 //////////////////////////////////////////////////////////////////
 // clicking on a 'more' section
 atom_1_strip_1.on(Events.TouchEnd, function(event, layer) {
-    if (scrolling) return;
-    console.log("click on 1.1");
-    container.visible = true;
-    container.index=1000;
-    event.stopPropagation();
-    atom_1_1.superLayer=scroller.content;
+if (scrolling) return;
+console.log("click on 1.1");
+container.visible = true;
+container.index=1000;
+event.stopPropagation();
+atom_1_1.superLayer=scroller.content;
 
 });
 
 atom_2_strip_1.on(Events.TouchEnd, function(event, layer) {
-    if (scrolling) return;
-    container.visible = true;
-    container.index=1000;
-    event.stopPropagation();
-    atom_2_1.superLayer=scroller.content;
+if (scrolling) return;
+container.visible = true;
+container.index=1000;
+event.stopPropagation();
+atom_2_1.superLayer=scroller.content;
 });
 
 //popups
 atom_2_strip_3.on(Events.TouchEnd, function(event, layer) {
-    if (scrolling) return;
-    pop_2_1.visible = true;
-    pop_2_1.index = 1000;
-    event.stopPropagation();
+if (scrolling) return;
+pop_2_1.visible = true;
+pop_2_1.index = 1000;
+event.stopPropagation();
 });
 pop_2_1.on(Events.TouchEnd, function(event, layer) {
-    pop_2_1.visible = false;
-    pop_2_1.index = 1000;
-    event.stopPropagation();
+pop_2_1.visible = false;
+pop_2_1.index = 1000;
+event.stopPropagation();
 });
 
 atom_2_strip_2.on(Events.TouchEnd, function(event, layer) {
-    if (scrolling) return;
-    pop_2_2.visible = true;
-    pop_2_2.index = 1000;
-    event.stopPropagation();
+if (scrolling) return;
+pop_2_2.visible = true;
+pop_2_2.index = 1000;
+event.stopPropagation();
 });
 pop_2_2.on(Events.TouchEnd, function(event, layer) {
-    pop_2_2.visible = false;
-    pop_2_2.index = 1000;
-    event.stopPropagation();
+pop_2_2.visible = false;
+pop_2_2.index = 1000;
+event.stopPropagation();
 });
 
 atom_3_hotspot = new Layer({x:740, y:1600, width:600, height:150, backgroundColor: "transparent", superLayer: atom_3});
 atom_3_hotspot.on(Events.Click, function(event, layer) {
-    pop_3_1.visible = true;
-    pop_3_1.index = 1000;
-    event.stopPropagation();
+pop_3_1.visible = true;
+pop_3_1.index = 1000;
+event.stopPropagation();
 });
 pop_3_1.on(Events.Click, function(event, layer) {
-    pop_3_1.visible = false;
-    pop_3_1.index = 1000;
-    event.stopPropagation();
+pop_3_1.visible = false;
+pop_3_1.index = 1000;
+event.stopPropagation();
 });
 
 
 //////////////////////////////////////////////////////////////////
 // set up scrollers
-    var scroller = new Framer.ScrollComponent({
-        width: 1320,
-        height: 1900,
-        scrollVertical: true,
-        scrollHorizontal: false,
-        y: 100,
-        superLayer: container
-    });
+var scroller = new Framer.ScrollComponent({
+width: 1320,
+height: 1900,
+scrollVertical: true,
+scrollHorizontal: false,
+y: 100,
+superLayer: container
+});
 
 close_button = new Layer({x:0, y:2040, width:1320, height:200, backgroundColor: "transparent", superLayer: container});
 close_button.on(Events.TouchEnd, function(event, layer) {
