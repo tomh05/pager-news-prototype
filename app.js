972436698ab6f6730789602df48ee58ce78f2c0a
Tom <tom.howe@bbc.co.uk>
2015-09-08 11:51:03 +0100

    changing assets, page indicator

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var SpineElement = function(spineElementNo,mainImgSrc) {
+var SpineElement = function(spineElementNo,foregroundImgSrc,backroundImgSrc) {
     //this.name = name;
-    this.layer = new Layer({x:0, y:0, width:1440, height:2392, image:mainImgSrc});
+    this.foregroundLayer = new Layer({
+        x:0, y:0,
+    width:1135, height:342,
+    image:foregroundImgSrc
+    });
+    this.backgroundLayer = new Layer({
+        x:0, y:0,
+        width:1440, height:2304,
+        image:backroundImgSrc
+    });
+    this.backgroundLayer.visible = false;
+
     this.moreItems = new Array();
     this.popups = new Array();
     this.spineElementNo = spineElementNo;
@@ -19,6 +30,7 @@ var SpineElement = function(spineElementNo,mainImgSrc) {
         directionLock: true,
         backgroundColor: "transparent",
         directionLockThreshold: {x:10,y:10},
+        contentInset: {top:100,right:100,bottom:100,left:100},
         superLayer: this.layer
     }); 
     this.drawer.on(Events.DirectionLockDidStart, function(event, layer) {  scrolling = true; });
@@ -75,7 +87,16 @@ SpineElement.prototype.launchMoreBox = function(destPage) {
 
 
 var MoreButton = function(spineElementNo,destBox,buttonImg) {
-    this.layer = new Layer({x:0, y:0, width:1027, height:600, image:buttonImg});
+    this.layer = new Layer({x:0, y:0, 
+        width:1200, height:600, 
+        backgroundColor: "transparent"
+    });
+    this.sublayer = new Layer({
+        x:50, y:50, 
+        width:1027, height:600, 
+        image:buttonImg,
+        superLayer:this.layer
+    });
     this.layer.spineElementNo = spineElementNo;
     this.layer.destBox = destBox;
     //this.contentImg = contentImg;
@@ -156,9 +177,14 @@ var MoreSection = function(moreItems,startingElementNo) {
 };
 
 var spineElements = new Array();
-spineElements.push( new SpineElement(0,"images/01.jpg") ); 
-spineElements.push( new SpineElement(1,"images/02.jpg") ); 
-spineElements.push( new SpineElement(2,"images/03.jpg") ); 
+spineElements.push( new SpineElement(0,"images/text_1.png","images/bg_01.jpg") ); 
+spineElements.push( new SpineElement(1,"images/text_2.png","images/bg_02.jpg") ); 
+spineElements.push( new SpineElement(2,"images/text_3.png","images/bg_03.jpg") ); 
+spineElements.push( new SpineElement(3,"images/text_4.png","images/bg_04.jpg") ); 
+spineElements.push( new SpineElement(4,"images/text_5.png","images/bg_05.jpg") ); 
+spineElements.push( new SpineElement(5,"images/text_6.png","images/bg_06.jpg") ); 
+spineElements.push( new SpineElement(6,"images/text_7.png","images/bg_07.jpg") ); 
+spineElements.push( new SpineElement(7,"images/text_8.png","images/bg_08.jpg") ); 
 
 spineElements[0].addMoreItem("images/1.1.jpg","images/01_strip_1.jpg");
 spineElements[0].addMoreItem("images/1.1.jpg", "images/01_strip_2.jpg");
@@ -170,23 +196,73 @@ spineElements[1].addMoreItem("images/POP_2.2.jpg", "images/02_strip_3.jpg");
 //////////////////////////////////////////////////////////////////
 var scrolling = false;
 
-var mainPager = new Framer.PageComponent({
-    width: 1440,
-    height: 2392,//1720,//2392,
-    scrollVertical: false,
-    scrollHorizontal: true,
-    y: 0//,
+
+var screen = new Layer({
+    x:0, y:0,
+     width: 1440,
+    height: 2392,
+    backgroundColor: "transparent"
 });
 
+var mainPager = new Framer.PageComponent({
+    width: 1440,
+    height: 2392,
+    scrollVertical: false,
+    scrollHorizontal: true,
+    y: 0,
+    superLayer: screen
+});
+
+pageIndicators = new Array();
+numPages = spineElements.length;
+pageIndicatorHolder = new Layer({
+    y:50,
+    width: (34+30) * numPages,
+                    height:40,
+                    backgroundColor: "transparent",
+                    superLayer: screen
+});
+pageIndicatorHolder.centerX();
 
 spineElements.forEach(function(spineElement,index,array){
-    mainPager.addPage(spineElement.layer,"right");
-
-
+    mainPager.addPage(spineElement.foregroundLayer,"right");
     //populate drawers
     //spineElement.moreItems.forEach(function(moreItem,index2,array2){
     //    spineElement.drawer.addPage(moreItem.buttonLayer,"right");
     //});
-
     //spineElement.populateMoreSections();
+    pageIndicator = new Layer({
+        width: 34,
+                  height: 34,
+                  y:0,
+                  x: (34+30)*index,
+        borderRadius: 17,
+        //backgroundColor: "#FFFFFF"
+        image: "images/dot_no_fill.png",
+        superLayer:pageIndicatorHolder
+
+    });
+    //pageIndicator.style.border = "5px solid #d2d2d2";
+    pageIndicator.states.add({
+        filled: { image: "images/dot_fill.png" },
+    });
+    pageIndicators.push(pageIndicator);
+});
+
+pageIndicators[0].states.switch("filled");
+
+
+mainPager.on("change:currentPage",function(event,layer) {
+    console.log("changed");
+    var index = mainPager.horizontalPageIndex(mainPager.currentPage);
+
+    for (var i =0;i<pageIndicators.length;i++) {
+        if (i<=index) {
+            pageIndicators[i].states.switch("filled");
+        } else {
+
+            pageIndicators[i].states.switch("default");
+        }
+    }
+
 });
